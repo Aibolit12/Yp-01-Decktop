2ba12b9009b79cf3dfce0dde4741153a2cecc29b
Aleksandr <ialeksandr2004i@gmail.com>
2024-03-16 15:10:52 +0500

    Фикс класса DataBase

--- Yp-01-Decktop/Classes/DataBase.cs
+++ Yp-01-Decktop/Classes/DataBase.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Yp_01_Decktop.Classes
 {
-    internal class DataBase
+    public class DataBase
     {
+        public static DataTable Select(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("database");
+            SqlConnection sqlConnection = new SqlConnection("server = DESKTOP-60TQI18; Trusted_Connection = No; DataBase =YP-01; User =sa; PWD =sa");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
     }
 }
