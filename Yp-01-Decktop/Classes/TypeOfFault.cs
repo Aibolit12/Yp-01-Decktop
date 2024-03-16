78940e5f29c94af9167392dd20719c85ecb948db
Aleksandr <ialeksandr2004i@gmail.com>
2024-03-16 14:37:55 +0500

    Добавьте файлы проекта.

---
+++ Yp-01-Decktop/Classes/TypeOfFault.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yp_01_Decktop.Classes
+{
+    internal class TypeOfFault
+    {
+    }
+}
