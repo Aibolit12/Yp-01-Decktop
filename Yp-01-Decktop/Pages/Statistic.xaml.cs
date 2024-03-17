adc0469d8fba945cd6246aba5db6f0f833889afd
Aleksandr <ialeksandr2004i@gmail.com>
2024-03-17 23:18:52 +0500

    Сделал вычисление среднего времени выполения заявки и количество выполненных заявок

--- Yp-01-Decktop/Pages/Statistic.xaml.cs
+++ Yp-01-Decktop/Pages/Statistic.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Yp_01_Decktop.Classes;
 
 namespace Yp_01_Decktop.Pages
 {
@@ -26,8 +27,21 @@ namespace Yp_01_Decktop.Pages
         {
             InitializeComponent();
             mainWindow = _mainWindow;
-            DataTable query = Classes.DataBase.Select("SELECT COUNT(*) FROM Requests;");
+            DataTable query = Classes.DataBase.Select($"SELECT COUNT(*) FROM Requests where Status = '{2}';");
             CountRequest.Content = "Количество выполненных заявок: " + query.Rows[0][0].ToString();
+            int complete = 0;
+            TimeSpan totalTime = new TimeSpan();
+            foreach (var row in mainWindow.RequestItem)
+            {
+                if (row.Status == "2")
+                {
+                    complete++;
+                    totalTime += DateTime.Parse(row.EndDate) - DateTime.Parse(row.StartDate);
+                }
+            }
+            if (totalTime.Days == 0) AvarageTime.Content = "Среднее время выполнения заявки: " + totalTime.Days;
+            else AvarageTime.Content = "Среднее время выполнения заявки: " + totalTime.Days / complete + " День";
+
         }
         public void TransitionBack(object sender, RoutedEventArgs e)
         {
