fbf444f9c1d4ce936e07d8eb455ab8a365648efc
Aleksandr <ialeksandr2004i@gmail.com>
2024-03-17 14:51:08 +0500

    Добавил добавление заявки в бд

--- Yp-01-Decktop/Pages/RequestEdit.xaml.cs
+++ Yp-01-Decktop/Pages/RequestEdit.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Yp_01_Decktop.Classes;
 
 namespace Yp_01_Decktop.Pages
 {
@@ -25,6 +27,21 @@ namespace Yp_01_Decktop.Pages
         {
             InitializeComponent();
             mainWindow = _mainWindow;
+            LoadTypeOfFaul();
+        }
+        public void LoadTypeOfFaul()
+        {
+            DataTable item = Classes.DataBase.Select($"select * from [TypeOfFault]");
+            foreach (DataRow row in item.Rows)
+            {
+                Classes.TypeOfFault typeOfFault = new Classes.TypeOfFault
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row["Name"].ToString()
+                };
+                mainWindow.TypeOfFaultList.Add(typeOfFault);
+                comboBoxTypesOfFaults.Items.Add(typeOfFault.Name);
+            }
         }
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -42,7 +59,45 @@ namespace Yp_01_Decktop.Pages
         }
         public void AddRequest(object sender, RoutedEventArgs e)
         {
-            mainWindow.frame.Navigate(new Pages.Main(mainWindow));
+            if (Equipment.Text.Length != 0 && comboBoxTypesOfFaults.SelectedIndex != -1 && Description.Text.Length != 0)
+            {
+                try
+                {
+                    DateTime StartDate = DateTime.Now;
+                    Random rand = new Random();
+
+                    const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    Random random = new Random();
+                    string Number = new string(Enumerable.Repeat(allowedChars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
+                    int TypesOfFaults = 0;
+                    if (comboBoxTypesOfFaults.SelectedIndex == 0) TypesOfFaults = 1;
+                    else if (comboBoxTypesOfFaults.SelectedIndex == 1) TypesOfFaults = 2;
+                    else if (comboBoxTypesOfFaults.SelectedIndex == 2) TypesOfFaults = 3;
+                    else if (comboBoxTypesOfFaults.SelectedIndex == 3) TypesOfFaults = 4;
+                    else if (comboBoxTypesOfFaults.SelectedIndex == 4) TypesOfFaults = 5;
+                    DataTable result = Classes.DataBase.Select($"insert into [Requests] (Number,StartDate,Equipment,TypeOfFault,Description,Client,Status) values ('{Number}','{StartDate}','{Equipment.Text}','{TypesOfFaults}','{Description.Text}','{Users.Id}','{1}')");
+                    Classes.Request request = new Classes.Request
+                    {
+                        Number = Number,
+                        StartDate = StartDate.ToString(),
+                        Equipment = Equipment.Text,
+                        TypeOfFault = TypesOfFaults.ToString(),
+                        Description = Description.Text,
+                        Client = Users.Id.ToString(),
+                        Status = 1.ToString(),
+                    };
+                    mainWindow.RequestItem.Add(request);
+                    mainWindow.LoadItem();
+                    MessageBox.Show("Запись успешно добавлена");
+                    mainWindow.frame.Navigate(new Pages.Main(mainWindow));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else MessageBox.Show("Заполните все поля");
+            
         }
         public void TransitionBack(object sender, RoutedEventArgs e)
         {
